75c786ae22906062f8aa9fe81adee91f5b2d7510
Popov Roman <popovrromma@gmail.com>
2024-12-09 23:52:25 +1000

    add docker compose

--- db/tables.py
+++ db/tables.py
@@ -1,4 +1,9 @@
-from sqlalchemy import Column, Integer, String, Boolean, Text
+from sqlalchemy import Column, Integer, String, Boolean, Text, DOUBLE
 from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
+class Products(Base):
+    __tablename__ = "Products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    price = Column(DOUBLE)
