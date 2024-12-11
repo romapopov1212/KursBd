30fe79f57fb0a3e05bd2d10578d53afbf2795021
Popov Roman <popovrromma@gmail.com>
2024-12-12 00:30:17 +1000

    add update product(without checks)

--- api/products.py
+++ api/products.py
@@ -13,3 +13,11 @@ async def add_product(
         service: ProductsService=Depends()
 ):
     return await service.add_product(prod_data)
+
+@router.patch("/update_product/{product_id}")
+async def update_product(
+        product_id:int,
+        new_product_data: Product,
+        service: ProductsService=Depends(),
+):
+    return await service.update_product(product_id, new_product_data)
--- services/products.py
+++ services/products.py
@@ -1,7 +1,13 @@
+from copy import deepcopy
+
+from fastapi import HTTPException
+from itertools import product
+
 from sqlalchemy.orm import Session
 from fastapi import Depends
 from fastapi.responses import JSONResponse
 from fastapi import status
+from sqlalchemy.future import select
 
 from models.products import Product
 from database import get_session
@@ -29,4 +35,42 @@ class ProductsService:
             status_code= status.HTTP_200_OK,
             content={"message": f"Товар '{product_data.name}' добален"}
         )
+
+    async def update_product(
+            self,
+            product_id,
+            new_data: Product
+    ):
+        prod = await self.get_product(product_id)
+        # old_prod = deepcopy(prod)
+        # if prod in None:
+        #     return HTTPException(
+        #         status_code=status.HTTP_404_NOT_FOUND,
+        #         detail=f"Товар не найден"
+        #     )
+        for field, value in vars(new_data).items():
+            setattr(prod, field, value)
+        await self.session.commit()
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"message" : f"Товар '{prod.name}' изменен"}
+        )
+
+    # async def get_product_by_name(
+    #         self,
+    #         name:str
+    # ) -> tables.Products:
+    #     prod = select(tables.Products).filter(tables.Products.name==name)
+    #     result = await self.session.execute(prod)
+    #     product_by_name = result.scalars().first()
+    #     return product_by_name
+
+    async def get_product(
+            self,
+            anything,
+    ) -> tables.Products:
+        prod = select(tables.Products).filter(tables.Products.id==anything).first()
+        result = await self.session.execute(prod)
+        product_by_id = result.scalars().first()
+        return product_by_id
 
