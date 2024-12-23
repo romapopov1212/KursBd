40e689260f89cb91f07b45b763c177f48a3df87e
Popov Roman <popovrromma@gmail.com>
2024-12-23 20:19:25 +1000

    add validator telephone number

--- models/buyers.py
+++ models/buyers.py
@@ -1,7 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 class Buyers(BaseModel):
     telephone_number: str
     firstname: str
     surname: str
     lastname: str
+
+    @field_validator("telephone_number")
+    def validate_telephone_number(cls, v):
+        if not v.startswith('8') or not v[1:].isdigit() or len(v) != 11:
+            raise ValueError("Номер телефона должен начинаться с '8' и содержать 11 цифр")
+        return v
