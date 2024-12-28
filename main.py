954de650acc19be61f9fa3feb79b2db364a5d9b3
Popov Roman <popovrromma@gmail.com>
2024-12-28 16:52:39 +1000

    start front

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from database import engine
 from api import router
@@ -8,8 +10,16 @@ async def init_db():
         await conn.run_sync(tables.Base.metadata.create_all)
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+templates = Jinja2Templates(directory="frontend")
+
 @app.on_event("startup")
 async def startup_event():
     await init_db()
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("html/first_page.html", {"request": request})
 
 app.include_router(router)
