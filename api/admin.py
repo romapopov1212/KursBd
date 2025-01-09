a0b9b3db5f6830cd184f36c0088d8e381acf20c7
Popov Roman <popovrromma@gmail.com>
2025-01-09 23:21:22 +1000

    add schemes for login admin and add form for login admin(frontend)

--- api/admin.py
+++ api/admin.py
@@ -1,14 +1,19 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form
 from services.admin_token import AdminService
+from pydantic import BaseModel
 router = APIRouter(
     prefix="/shop/admin",
     tags=['admin']
 )
 
+
+class AdminLogin(BaseModel):
+    admin_name: str
+    admin_password: str
+
 @router.post("/login")
 async def login(
-        admin_name: str,
-        admin_password: str,
+        loginData: AdminLogin,
         service: AdminService = Depends()
 ):
-    return await service.login_admin(admin_name, admin_password)
+    return await service.login_admin(loginData.admin_name, loginData.admin_password)
