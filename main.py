1c4ff59eb5cc16eebe57e63c2442705af405abf5
Popov Roman <popovrromma@gmail.com>
2025-01-09 17:35:36 +1000

    fix front and add button for login administrator

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import engine
 from api import router
 from db import tables
+
+
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(tables.Base.metadata.create_all)
 app = FastAPI()
 
+app.include_router(router)
+
+app.mount('/static', StaticFiles(directory='static'), 'static')
+
+origins = [
+    'http://127.0.0.1:8000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.on_event("startup")
 async def startup_event():
     await init_db()
 
-app.include_router(router)
+@app.get("/")
+async def root():
+    return FileResponse('templates/index.html')
