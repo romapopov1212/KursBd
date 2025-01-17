6f2ff0244f7a7ac2e78298e46fe01e25590b26e0
Popov Roman <popovrromma@gmail.com>
2025-01-17 22:10:21 +1000

    add buyers page

--- api/buyers.py
+++ api/buyers.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, Request
 from fastapi import Depends
 from fastapi.security import HTTPAuthorizationCredentials
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
 
 from services.admin_token import AdminService
 from services.buyers import http_bearer
 from models.buyers import Buyers
 from services.buyers import BuyersService
+
+templates = Jinja2Templates(directory="templates")
 
 router = APIRouter(
     prefix="/shop/buyers",
@@ -19,7 +24,7 @@ async def add_buyer(
 ):
     return await service.add_buyers(buyer_data)
 
-@router.delete("/buyer/delete")
+@router.delete("/buyer/delete/{buyer_number}")
 async def delete(
         buyer_number: str,
         service: BuyersService=Depends(),
@@ -39,3 +44,13 @@ async def get_all_buyers(
 ):
     t = admin_serice.get_cooks_and_check_is_admin(request=request)
     return await service.get_list(t)
+
+@router.get("/buyers_page", response_class=HTMLResponse)
+async def get_page(
+    request: Request,
+    service: BuyersService = Depends(),
+    admin_service: AdminService = Depends()
+):
+    t = admin_service.get_cooks_and_check_is_admin(request=request)
+    buyers = await service.get_list(t)
+    return templates.TemplateResponse("buyer.html", {"request" : request, "buyers" : buyers})
